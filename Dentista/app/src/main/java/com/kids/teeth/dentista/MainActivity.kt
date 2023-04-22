eb83e7d3d21688582977dc0e23cc01ea5d4fb93b
Guilherme Azevedo <gui.azevedo@ymail.com>
2023-04-22 12:24:17 -0300

    dentista - first commit 22/04/2023

---
+++ Dentista/app/src/main/java/com/kids/teeth/dentista/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.kids.teeth.dentista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+
+    }
+}
