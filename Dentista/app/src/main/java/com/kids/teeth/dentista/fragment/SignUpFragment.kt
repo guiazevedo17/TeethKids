80b2c6e7e878334a4c16d4b562bdb73ee91f74cd
Jzanholo <joaozanholo2002@hotmail.com>
2023-04-27 18:35:20 -0300

    sprint2_autenticacao - autenticacao de login e cadastro OK 27/04/2023

--- Dentista/app/src/main/java/com/kids/teeth/dentista/fragment/SignUpFragment.kt
+++ Dentista/app/src/main/java/com/kids/teeth/dentista/fragment/SignUpFragment.kt
@@ -1,6 +1,7 @@
 package com.kids.teeth.dentista.fragment
 
 import android.content.ContentValues
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,6 +10,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -24,6 +28,7 @@ class SignUpFragment : Fragment() {
 
     private lateinit var db: FirebaseFirestore
 
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -37,8 +42,14 @@ class SignUpFragment : Fragment() {
             )
         return binding.root
     }
+    override fun onStart() {
+        super.onStart()
+        db = FirebaseFirestore.getInstance(Firebase.app)
+        auth = FirebaseAuth.getInstance(Firebase.app)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnAddressSignUp.setOnClickListener {
@@ -50,32 +61,77 @@ class SignUpFragment : Fragment() {
         }
 
         binding.btnConcludeSignUp.setOnClickListener {
+            val Name = binding.etNameSignUp.text.toString()
+            val Phone = binding.etPhoneSignUp.text.toString()
+            val Email = binding.etEmailSignUp.text.toString()
+            val Password = binding.etPasswordSignUp.text.toString()
+            val PasswordConfirm = binding.etConfPasswordSignUp.text.toString()
+            if(confirmPassword(Password,PasswordConfirm)){
+
+                if(fieldNotNull(Name, Phone, Email, Password, PasswordConfirm)){
+
+                    auth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener { register ->
+                        if (register.isSuccessful) {
+                            val snackbar = Snackbar.make(view, "Sucesso ao cadastrar usuario!", Snackbar.LENGTH_SHORT)
+                            snackbar.setBackgroundTint(Color.BLUE)
+                            snackbar.show()
+                            registerAccount(Name,Phone,Email,Password)
+                            clearFields()
+                        }
+                    }
+                }
+                else{
+                    val snackbar = Snackbar.make(view, "Preencha todos os campos!", Snackbar.LENGTH_SHORT)
+                    snackbar.setBackgroundTint(Color.RED)
+                    snackbar.show()
+                }
+            }
+            else{
+                val snackbar = Snackbar.make(view, "As senhas não correspondem!", Snackbar.LENGTH_SHORT)
+                snackbar.setBackgroundTint(Color.RED)
+                snackbar.show()
+            }
             //register()
         }
 
     }
 
-    override fun onStart() {
-        super.onStart()
-        db = FirebaseFirestore.getInstance(Firebase.app)
+
+
+    // Limpa os campos
+    private fun clearFields(){
+        binding.etNameSignUp.setText("")
+        binding.etEmailSignUp.setText("")
+        binding.etPhoneSignUp.setText("")
+        binding.etPasswordSignUp.setText("")
+        binding.etConfPasswordSignUp.setText("")
     }
 
-    private fun register() {
-        val fieldName = binding.etNameSignUp
-        val fieldPhone = binding.etPhoneSignUp
-        val fieldEmail = binding.etEmailSignUp
-        val fieldPassword = binding.etPasswordSignUp
+    // Verifica compatibilidade das senhas
+    private fun confirmPassword(Password: String, PasswordConfirm: String): Boolean {
 
-        val name = fieldName.text.toString()
-        val phone = fieldPhone.text.toString()
-        val email = fieldEmail.text.toString()
-        val password = fieldPassword.text.toString()
+        if(Password == PasswordConfirm) {
+            return true
+        }
+        return false
+    }
+
+    // Verifica se existe algum campo nulo
+    private fun fieldNotNull(Name: String, Phone: String, Email: String, Password: String, PasswordConfirm: String): Boolean {
+
+        if(Name.isEmpty() || Phone.isEmpty() || Email.isEmpty() || Password.isEmpty() || PasswordConfirm.isEmpty()){
+            return false
+        }
+        return true
+    }
+
+    private fun registerAccount(Name: String, Phone: String, Email: String, Password: String) {
 
         val dentist = hashMapOf(
-            "name" to name,
-            "phone" to phone,
-            "email" to email,
-            "password" to password
+            "name" to Name,
+            "phone" to Phone,
+            "email" to Email,
+            "password" to Password
         )
 
         db.collection("dentists")
@@ -86,6 +142,8 @@ class SignUpFragment : Fragment() {
             .addOnFailureListener { e ->
                 Log.w(ContentValues.TAG, "Error adding document", e)
             }
+
     }
+
 
 }
