456bad7bcff0b5f650e39dd2f1f8b2bac2210561
blemess <brunolemess@hotmail.com>
2023-06-09 10:27:49 -0300

    Functions Arrumada

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -42,20 +42,31 @@ export const setUser = functions
       .set(userData);
   });
 
+export const setToken = functions
+  .region("southamerica-east1")
+  .runWith({enforceAppCheck: false})
+  .https
+  .onCall(async (data, context) =>{
+    const {userId, ...fcmToken} = data;
+    await firebase.firestore()
+      .collection("dentist")
+      .doc(userId)
+      .update({"fcmToken": fcmToken});
+  });
 
 export const emergencyListener2 = functions
   .region("southamerica-east1")
   .runWith({enforceAppCheck: false})
   .firestore
-  .document("/teste_emergencia/{id}")
+  .document("/emergencies/{id}")
   .onUpdate(async (change, context) => {
     const newValue = change.after.data();
     const oldValue = change.before.data();
     const docId = context.params.id;
-    const newStatus = newValue.status;
-    const oldStatus = oldValue.status;
+    const newStatus = newValue.state;
+    const oldStatus = oldValue.state;
 
-    if (newStatus === true && newStatus !== oldStatus) {
+    if (newStatus === "new" && newStatus !== oldStatus) {
       const querySnapshot = await firebase.firestore()
         .collection("dentists")
         .where("availability", "==", true)
@@ -74,9 +85,29 @@ export const emergencyListener2 = functions
       } catch (e) {
         console.error("Erro ao enviar a notificação", e);
       }
-      // Restante do código...
     } else {
       console.log("O valor de status não foi modificado ");
+    }
+  });
+
+export const sendMessage = functions
+  .region("southamerica-east1")
+  .runWith({enforceAppCheck: false})
+  .https
+  .onRequest(async (data, context)=>{
+    // eslint-disable-next-line max-len
+    const fcmToken = "cqmeff5FQfGNpW2YPkL3yr:APA91bGG87ayBWtQTLXbo2Mqa2WxoS0WLmFDRtMwfDf-VTejmmcfTfO4k8lozRVDNKYlpolEB8C268uYVg7oiqniICeVzXRaNHWV5nzSFcVddU5Z8s9iJtfs5m7ShEZ53BAA5l9Clcz0";
+    const message = {
+      data: {
+        text: "Teste",
+        id: "GgieUDQz4UMUgGVU3aoEAplZ1Zf2",
+      },
+      token: fcmToken,
+    };
+    try {
+      await firebase.messaging().send(message);
+    } catch (e) {
+      console.error("Erro ao enviar a notificação", e);
     }
   });
 
