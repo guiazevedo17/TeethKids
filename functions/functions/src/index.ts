8ad32e9d13695f1d8ecde25cdb293da39d266b32
blemess <brunolemess@hotmail.com>
2023-06-15 01:02:46 -0300

    functions update_1

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
@@ -47,9 +48,11 @@ export const setToken = functions
   .runWith({enforceAppCheck: false})
   .https
   .onCall(async (data, context) =>{
-    const {userId, ...fcmToken} = data;
+    const userId = data["userId"];
+    const fcmToken = data["fcmToken"];
+    const collection = data["collection"];
     await firebase.firestore()
-      .collection("dentist")
+      .collection(collection)
       .doc(userId)
       .update({"fcmToken": fcmToken});
   });
@@ -63,6 +66,7 @@ export const emergencyListener2 = functions
     const newValue = change.after.data();
     const oldValue = change.before.data();
     const docId = context.params.id;
+
     const newStatus = newValue.state;
     const oldStatus = oldValue.state;
 
@@ -71,10 +75,11 @@ export const emergencyListener2 = functions
         .collection("dentists")
         .where("availability", "==", true)
         .get();
+
       const tokens = querySnapshot.docs.map((doc)=>doc.data().fcmToken);
       const message ={
         data: {
-          text: newValue.name,
+          text: "emergencyUpdated",
           id: docId,
         },
         tokens: tokens,
@@ -90,17 +95,19 @@ export const emergencyListener2 = functions
     }
   });
 
+// Função para o dentista enviar aceite
 export const sendMessage = functions
   .region("southamerica-east1")
   .runWith({enforceAppCheck: false})
   .https
-  .onRequest(async (data, context)=>{
+  .onCall(async (data, context)=>{
     // eslint-disable-next-line max-len
-    const fcmToken = "cqmeff5FQfGNpW2YPkL3yr:APA91bGG87ayBWtQTLXbo2Mqa2WxoS0WLmFDRtMwfDf-VTejmmcfTfO4k8lozRVDNKYlpolEB8C268uYVg7oiqniICeVzXRaNHWV5nzSFcVddU5Z8s9iJtfs5m7ShEZ53BAA5l9Clcz0";
+    const fcmToken = data["fcmToken"];
+    const dId = data["dentistId"];
     const message = {
       data: {
-        text: "Teste",
-        id: "GgieUDQz4UMUgGVU3aoEAplZ1Zf2",
+        text: "accepted",
+        id: dId,
       },
       token: fcmToken,
     };
@@ -111,3 +118,129 @@ export const sendMessage = functions
     }
   });
 
+// Função para mendar mensagem para o dentista de aceite
+export const sendMessageToDentist = functions
+  .region("southamerica-east1")
+  .runWith({enforceAppCheck: false})
+  .https
+  .onCall(async (data, context)=>{
+    const fcmToken = data["fcmToken"];
+    const messageType = data["messageType"];
+    const uid = data["uid"];
+    const message={
+      data: {
+        text: messageType,
+        id: uid,
+      },
+      token: fcmToken,
+    };
+    try {
+      await firebase.messaging().send(message);
+    } catch (e) {
+      console.error("Erro ao enviar a mensagem", e);
+    }
+  });
+
+// Função para mandar mensagem de rating para o socorrista
+
+export const sendRTS = functions
+  .region("southamerica-east1")
+  .runWith({enforceAppCheck: false})
+  .https
+  .onCall(async (data, context)=>{
+    const fcmToken = data["fcmToken"];
+    const messageType= data["messageType"];
+    const dId = data["id"];
+    const message= {
+      data: {
+        text: messageType,
+        id: dId,
+      },
+      token: fcmToken,
+    };
+    await firebase.messaging().send(message);
+  });
+
+export const sendTestMessage2 = functions
+  .region("southamerica-east1")
+  .runWith({enforceAppCheck: false})
+  .https
+  .onRequest(async (data, context) =>{
+    const fcmToken = "fr-_DlKuSZKCzbbO-7HjDk:APA91bHmiK1EhT-vDzJDlHC1B6zbBK8ehjieeqkll0uhHCp_IOruBeeEZYY30TbaYvnVMY8OEGu_fPhMN_L36Ii_Ub2HPA-PtiPKYyl2zwJ4_ViqdmJ4GAqMKw8Zsiim7V7vk_o8T0X_";
+    const message = {
+      data: {
+        text: "Teste",
+        id: "TBvzC9u7tYOVv72s0zOIePpHX3C2",
+      },
+      token: fcmToken,
+    };
+    await firebase.messaging().send(message);
+  });
+
+export const setActions = functions
+  .region("southamerica-east1")
+  .runWith({enforceAppCheck: false})
+  .https
+  .onCall(async (data, context) => {
+    const docData = data;
+    docData.date = admin.firestore.Timestamp.now();
+
+    await firebase.firestore()
+      .collection("actions").doc().set(docData);
+  });
+
+export const setAddress = functions
+  .region("southamerica-east1")
+  .runWith({enforceAppCheck: false})
+  .https
+  .onCall(async (data, context) =>{
+    await firebase.firestore()
+      .collection("address")
+      .doc().set(data);
+  });
+
+// mudar retaing - review
+
+// criar localização
+export const sendLocation = functions
+  .region("southamerica-east1")
+  .runWith({enforceAppCheck: false})
+  .https
+  .onCall(async (data, context)=>{
+    const latitude = data.latLng.latitude;
+    const longitude = data.latLng.longitude;
+    const fcmToken = data.fcmToken;
+    const text = "location";
+
+    const message = {
+      data: {
+        text: text,
+        latitude: latitude,
+        longitude: longitude,
+      },
+      token: fcmToken,
+    };
+
+    await firebase.messaging().send(message);
+  });
+
+export const sendRating = functions
+  .region("southamerica-east1")
+  .runWith({enforceAppCheck: false})
+  .https
+  .onCall(async (data, context) =>{
+    const collectionRef = firebase.firestore().collection("reviews");
+
+    const collectionSnapshot = await collectionRef.get();
+
+    if (collectionSnapshot.empty) {
+      collectionRef.add({});
+    }
+
+    const docData = data;
+    docData.date = admin.firestore.Timestamp.now();
+    await firebase.firestore().collection("reviews").doc().set(docData);
+    return {
+      message: "Rating enviado com sucesso!",
+    };
+  });
