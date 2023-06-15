e2ac1725ccbee3f092d480dba62d60208dc0e743
Guilherme Azevedo <gui.azevedo@ymail.com>
2023-06-15 05:05:54 -0300

    socorrista - ok 15/06/2023

--- Dentista/app/src/main/java/com/kids/teeth/dentista/model/Emergency.kt
+++
@@ -1,8 +0,0 @@
-package com.kids.teeth.dentista.model
-
-data class Emergency (
-    val requesterName : String? = null,
-    val requesterPhone : String? = null,
-    val status : String? = null,
-    val date : String? = null
-)
